723e9f5fd32e4ef774e0025c836498ce85a177a7
Joel Wiklund <support@joelwiklund.se>
2026-03-03 14:40:06 +0100

    05. 7. Creating an activity details view

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,22 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityDetail from "../ActivityDetail";
 
 type Props = {
     activities: Activity[];
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({ activities }: Props) {
     return (
-        <div>
-            <Grid>
-                <ActivityList activities={activities} />
+        <>
+            <Grid container spacing={3}>
+                <Grid size={7}>
+                    <ActivityList activities={activities} />
+                </Grid>
+                <Grid size={5}>
+                    {activities[0] && <ActivityDetail activity={activities[0]} />}
+                </Grid>
             </Grid>
-        </div>
+        </>
     )
 }
